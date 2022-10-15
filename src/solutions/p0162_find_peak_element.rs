9a4f46317a912c262603cd75feb652cc97178a3f
qberg <bs17b025@smail.iitm.ac.in>
2022-10-15 06:45:41 +0530

    * Solved p162

--- src/solutions/p0162_find_peak_element.rs
+++ src/solutions/p0162_find_peak_element.rs
@@ -1,23 +1,22 @@
 pub struct Solution {}
 
 impl Solution {
-    pub fn find_peak_one_dim (arr: &[i32]) -> i32 {
-        let n = arr.len();
+    pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
         if n==1 {
             return 0;
-        } else if n==2 {
-            if arr[0] > arr[1] {
-                return 0;
-            } else {
-                return 1;
-            }
-        } else if arr[n/2 - 1] > arr[n/2] {
-            return Solution::find_peak_one_dim(&arr[..n/2]);
-        } else if arr[n/2+1] > arr[n/2] {
-            return Solution::find_peak_one_dim(&arr[n/2..]);
-        } else {
-            return (n/2) as i32;
         }
+        let (mut low, mut high) = (0_usize, n - 1);
+        let mut mid = 0;
+        while low < high {
+            mid = (high - low) / 2 + low;
+            if mid + 1 < n && nums[mid] < nums[mid + 1] {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        low as i32
     }
 }
 
