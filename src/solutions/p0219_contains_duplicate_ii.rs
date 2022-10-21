7cf4f04a312ab2e5a5e293024ef12f4679fe258d
qberg <bs17b025@smail.iitm.ac.in>
2022-10-21 14:14:04 +0530

    * Used a hashmap to keep track of seen elements...

--- src/solutions/p0219_contains_duplicate_ii.rs
+++ src/solutions/p0219_contains_duplicate_ii.rs
@@ -1,6 +1,35 @@
+use std::collections::HashSet;
+
 pub struct Solution {}
 
+// Time: O(n)
+// Space: O(n)
 impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let n = nums.len();
+        let k = k as usize;
+        let mut seen = HashSet::with_capacity(n);
+        for i in 0..n {
+            if seen.contains(&nums[i]) {
+                return true;
+            } else {
+                seen.insert(nums[i]);
+                if seen.len() > k {
+                    seen.remove(&nums[i-k]);
+                }
+            }
+        }
+        false
+    }
+}
+
+
+
+
+// Time Limit Exceeded solution...
+pub struct SolutionOne {} 
+
+impl SolutionOne {
     pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
         let n = nums.len();
         for left in 0..n {
