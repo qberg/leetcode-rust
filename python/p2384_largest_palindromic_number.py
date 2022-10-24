888c7516c6aadf7d66ef85cf38cd64d7c1db078d
qberg <bs17b025@smail.iitm.ac.in>
2022-10-24 10:32:20 +0530

    * Use a dictionary to coun the occureneces of various digits...

--- python/p2384_largest_palindromic_number.py
+++ python/p2384_largest_palindromic_number.py
@@ -6,14 +6,3 @@ class Solution:
         res = ''.join(digit_count[c]//2 * c for c in '9876543210').lstrip('0')
         mid = max(digit_count[c]%2 * c for c in digit_count)
         return (res+mid+res[::-1]) or '0'
-
-
-
-
-
-
-
-
-
-
-
