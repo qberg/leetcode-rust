5f477f6f29c0a759018e11051ae50c2412b450d9
qberg <bs17b025@smail.iitm.ac.in>
2022-10-25 22:56:54 +0530

    * Implemented a solution using iterators...

--- src/solutions/p0004_median_of_sorted_arrays.rs
+++ src/solutions/p0004_median_of_sorted_arrays.rs
@@ -1,7 +1,54 @@
+// Solutions using iterators...
 pub struct Solution {}
 
+pub struct VecsIter {
+    vec1: Vec<i32>,
+    vec2: Vec<i32>,
+}
+
+impl VecsIter {
+    fn len(&self) -> usize {
+        self.vec1.len() + self.vec2.len()
+    }
+}
+
+
+impl Iterator for VecsIter {
+    type Item = i32;
+    fn next(&mut self) -> Option<Self::Item> {
+        match (self.vec1.last(), self.vec2.last()) {
+            (Some(n1), Some(n2)) if n1 >= n2 => self.vec1.pop(),
+            (Some(_), Some(_))               => self.vec2.pop(),
+            (Some(_), _)                     => self.vec1.pop(),
+            (_, Some(_))                     => self.vec2.pop(),
+            _                                => None,
+        }
+    }
+}
 
 impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        let mut vecs_iter = VecsIter {
+            vec1: nums1,
+            vec2: nums2,
+        };
+        let length = vecs_iter.len();
+        let middle = length / 2;
+        if 0 == length % 2 {
+            let mut halves = vecs_iter.skip(middle - 1);
+            (halves.next().unwrap() as f64 + halves.next().unwrap() as f64) / 2.0
+        } else {
+            vecs_iter.nth(middle).unwrap() as f64
+        }
+    }
+}
+
+
+// Solution that creates a new vector...
+pub struct SolutionOne {}
+
+
+impl SolutionOne {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
         let mut nums: Vec<i32> = vec![];
         let (mut p1, mut p2) = (0,0);
